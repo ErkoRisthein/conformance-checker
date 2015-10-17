e6715667b81dd49c49e3cff60661a52b714ad3dc
Erko Risthein <erko.risthein@transferwise.com>
2015-10-17 18:11:05 +0300

    Added xes parsing support and a sample xes input file

--- src/test/java/ee/ut/cs/modeling/checker/ConformanceCheckerSpec.java
+++ src/test/java/ee/ut/cs/modeling/checker/ConformanceCheckerSpec.java
@@ -2,6 +2,8 @@ package ee.ut.cs.modeling.checker;
 
 import fr.lip6.move.pnml.framework.hlapi.HLAPIRootClass;
 import fr.lip6.move.pnml.framework.utils.PNMLUtils;
+import org.deckfour.xes.in.XesXmlParser;
+import org.deckfour.xes.model.XLog;
 import org.junit.Test;
 
 import java.io.File;
@@ -15,7 +17,14 @@ public class ConformanceCheckerSpec {
 
 	@Test
 	public void testImportPnml() throws Exception {
-		File f = new File("simple_test.pnml");
+		File f = new File("exercise_ab.pnml");
 		HLAPIRootClass rc = PNMLUtils.importPnmlDocument(f, false);
+	}
+
+	@Test
+	public void testImportXes() throws Exception {
+		XesXmlParser parser = new XesXmlParser();
+		File f = new File("exercise_ab.xes");
+		XLog log = parser.parse(f).get(0);
 	}
 }
