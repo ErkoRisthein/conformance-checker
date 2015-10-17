603f7240d7fe610e46ff652d0c977a77a2a60f41
Robert Roosalu <robert.roosalu@gmail.com>
2015-10-17 18:30:23 +0300

    Getting the petri net components f rom .pnml file.

--- src/test/java/ee/ut/cs/modeling/checker/ConformanceCheckerSpec.java
+++ src/test/java/ee/ut/cs/modeling/checker/ConformanceCheckerSpec.java
@@ -2,9 +2,12 @@ package ee.ut.cs.modeling.checker;
 
 import fr.lip6.move.pnml.framework.hlapi.HLAPIRootClass;
 import fr.lip6.move.pnml.framework.utils.PNMLUtils;
+import fr.lip6.move.pnml.ptnet.hlapi.PetriNetDocHLAPI;
+import fr.lip6.move.pnml.ptnet.hlapi.PnObjectHLAPI;
 import org.junit.Test;
 
 import java.io.File;
+import java.util.List;
 
 public class ConformanceCheckerSpec {
 
@@ -17,5 +20,11 @@ public class ConformanceCheckerSpec {
 	public void testImportPnml() throws Exception {
 		File f = new File("simple_test.pnml");
 		HLAPIRootClass rc = PNMLUtils.importPnmlDocument(f, false);
+		// May also use getObjects_ArcHLAPI(), getObjects_TransitionHLAPI(), getObjects_PlaceHLAPI() here.
+		List<PnObjectHLAPI> petriNetComponents = ((PetriNetDocHLAPI) rc).getNetsHLAPI().get(0)
+				.getPagesHLAPI().get(0).getObjectsHLAPI();
+		
 	}
+
+
 }
