82fceb01af8cd37d48e960b61ed90e777024c897
Erko Risthein <erko.risthein@transferwise.com>
2015-10-27 02:45:56 +0200

    Lambda magic

--- src/test/java/ee/ut/cs/modeling/checker/PetriNetTestHelper.java
+++ src/test/java/ee/ut/cs/modeling/checker/PetriNetTestHelper.java
@@ -3,13 +3,13 @@ package ee.ut.cs.modeling.checker;
 import ee.ut.cs.modeling.checker.domain.eventlog.Event;
 import ee.ut.cs.modeling.checker.domain.eventlog.Trace;
 
+import static java.util.Arrays.stream;
+
 public class PetriNetTestHelper {
 
 	public static Trace trace(String... events) {
 		Trace trace = new Trace();
-		for (String event : events) {
-			trace.addEvent(new Event(event));
-		}
+		stream(events).forEach(event -> trace.addEvent(new Event(event)));
 		return trace;
 	}
 
